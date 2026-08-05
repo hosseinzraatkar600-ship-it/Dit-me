f2b1771e39fccbc73bda08e693df396fa0134dd7
hosseinzraatkar600-ship-it <hosseinzraatkar600@gmail.com>
2026-08-06 02:34:04 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,86 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const funText = document.getElementById("funText");
+const formBox = document.getElementById("formBox");
+const sendBtn = document.getElementById("sendBtn");
+
+let size = 1;
 
 const messages = [
   "Are you sure? 🥺",
-  "Think again... 💔",
-  "Come on 😭",
-  "Pretty please? 🌸",
-  "You know you want to 😏",
-  "Last chance 😂"
+  "Think again 😭",
+  "Please don't say no 😂",
+  "Come on... 💕",
+  "You know you want to 😌"
 ];
 
 let count = 0;
-let yesSize = 1;
 
-noBtn.addEventListener("click", () => {
-    if (count < messages.length) {
-        funText.innerText = messages[count];
-    }
 
-    count++;
+noBtn.onclick = function(){
 
-    yesSize += 0.2;
+  count++;
 
-    yesBtn.style.transform = `scale(${yesSize})`;
+  if(count < messages.length){
+    funText.innerHTML = messages[count];
+  }
 
-    noBtn.style.transform = `scale(${Math.max(0.2,1-count*0.15)})`;
+  size += 0.2;
+  yesBtn.style.transform = `scale(${size})`;
 
-    if(count > 6){
-        noBtn.style.display = "none";
-    }
-});
+  noBtn.style.transform =
+  `scale(${Math.max(0.3, 1 - count*0.15)})`;
 
-yesBtn.addEventListener("click", () => {
-    alert("Next step coming soon 💕");
-});
+  if(count >= 5){
+    noBtn.style.display = "none";
+    funText.innerHTML = "Okay you have to say YES now 😂❤️";
+  }
+
+};
+
+
+yesBtn.onclick = function(){
+
+  document.querySelector("#yesBtn").style.display="none";
+  document.querySelector("#noBtn").style.display="none";
+
+  funText.innerHTML =
+  "Yay! Let's plan it 💕";
+
+  formBox.style.display="block";
+
+};
+
+
+sendBtn.onclick = function(){
+
+  let day = document.getElementById("day").value;
+  let time = document.getElementById("time").value;
+  let place = document.getElementById("place").value;
+
+
+  if(day && time){
+
+    formBox.innerHTML =
+    `
+    <h2>
+    Okay, see you then 💕
+    </h2>
+    <p>
+    I'm happy about it 😊
+    </p>
+    `;
+
+    console.log({
+      day,
+      time,
+      place
+    });
+
+  } else {
+
+    alert("Please choose day and time ❤️");
+
+  }
+
+};
